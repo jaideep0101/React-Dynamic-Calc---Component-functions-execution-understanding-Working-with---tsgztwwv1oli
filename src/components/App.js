999737a983d0ccd97c901988fbecb9e2ee70822d
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2023-03-30 13:31:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
 const App = () => {
+const [input1,setInpu1]=useState("");
+const [input2,setInput2]=useState("");
+
+const handleChangeA=(e)=>{
+  setInpu1(Number(e.target.value));
+}
+const handleChangeB=(e)=>{
+  setInput2(Number(e.target.value))
+}
+const sum = input1+input2;
 
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id='input1' onChange={handleChangeA}/>
        +
-      <input id='input2' onChange={}/> 
+      <input id='input2' onChange={handleChangeB}/> 
       
-      <p id='result'></p>
+      <p id='result'>{sum}</p>
     </div>
   )
 }
